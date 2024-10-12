80c7db71f34a2ea457c89b7f5115dcd422160a73
yzd0014 <1051714925@qq.com>
2024-10-12 13:52:11 -0700

    proof for direct Euler angle comparison constraint

--- EulerTwistConstraint.m
+++ EulerTwistConstraint.m
@@ -1,6 +1,6 @@
 clear;
 import sym.*
-
+%normalized approach
 syms R Ry Rz [3 3]
 syms theta_y theta_z c_angle
 Rz=eye(3);
@@ -69,8 +69,41 @@ J1=-x.'*R.'*get_skew_symmetric(y)*get_skew_symmetric(R*z);
 J2=-cos(0.25*pi)/norm(s)*s.'*get_skew_symmetric(y)*get_skew_symmetric(R*x);
 J=J0+J1+J2;
 J
+%% 
+%direct angle approach
+clear;
+import sym.*
+syms R Ry Rx [3 3]
+syms theta_y theta_x
+Ry=eye(3);
+Ry(1,1)=cos(theta_y);
+Ry(1,3)=sin(theta_y);
+Ry(3,1)=-sin(theta_y);
+Ry(3,3)=cos(theta_y);
+Rx=eye(3);
+Rx(2,2)=cos(theta_x);
+Rx(2,3)=-sin(theta_x);
+Rx(3,2)=sin(theta_x);
+Rx(3,3)=cos(theta_x);
+% Ry
+% Rx
+R=Ry*Rx;
+syms x y z [3 1]
+x=zeros(3,1);
+x(1)=1;
+R*x
+
+B=R(2,3)*R(2,3)+R(2,2)*R(2,2);
+J1=(-R(3,3)*R(2,2)+R(3,2)*R(2,3))/B;
+J3=(R(1,3)*R(2,2)-R(1,2)*R(2,3))/B;
+J1
+J3
+
 function M = get_skew_symmetric(v)
    M = [0, -v(3), v(2);
      v(3), 0, -v(1);
      -v(2), v(1), 0];
 end
+
+
+
